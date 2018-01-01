34a79bb96026fc6f4ed041ccdade5ffe4a7a8e28
Rafael Fiume <rafael.fiume@yahoo.it>
2018-01-01 18:22:48 +0000

    Configures QuickCheck tests
        - Also adds a bit more doc on how to use QuickCheck
    
    @rafaelfiume

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 A simple Tic-Tac-Toe console game implemented in Haskell
 
 
-## Basic Haskell
+# Basic Haskell
 
 ### Stack
 
@@ -19,3 +19,13 @@ Examples of how to use GHCI when executing `stack ghci`:
     Prelude> :load Main
 
 More about GHCI [here](http://learnyouahaskell.com/starting-out)
+
+## Testing
+
+It might be useful to have a look at how [lens tests its code](https://github.com/ekmett/lens/tree/master/tests).
+
+### QuickCheck
+
+Useful resources:
+
+* [QuickCheck: An Automatic Testing Tool for Haskell](http://www.cse.chalmers.se/~rjmh/QuickCheck/manual.html)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,28 @@
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.QuickCheck
+
+import Playground.Learning
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests = [
+        testGroup "Basic properties examples (failing)" [
+                testProperty "prop1" prop1,
+                testProperty "prop2" prop2
+        ],
+        testGroup "Function properties (others coming soon)" [
+                testProperty "prop_RevRev" prop_RevRev
+        ]
+      ]
+
+prop1 b = b == False
+    where types = (b :: Bool)
+
+prop2 i = i == 42
+    where types = (i :: Int)
+
+
+prop_RevRev xs = reverse' (reverse' xs) == xs
+    where types = (xs :: [Int])
